cd40fdb0cc3017c24986b0e6580d7e690123bb74
GuilleRot <guillermoarotchyn@gmail.com>
2022-05-03 12:57:17 -0300

    Se agregan ejemplos de javascript

---
+++ JS/JavaScript_Arrow_Function/ArrowFunction.js
@@ -0,0 +1,13 @@
+
+let Name = "Name";
+let LastName = "LastName";
+
+console.log(`${Name} ${LastName}`);
+
+//Funcion Normal
+function myFunction(){
+
+}
+
+//Arrow Function
+const myArrowFunction = () => {};
